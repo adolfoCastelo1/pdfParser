ad1577ff778a851df98d7fc3edd3f6a9571f9f81
laptop1 <laptop1@LAPTOP>
2018-08-21 17:38:35 -0300

    Agregar archivos de proyecto.

---
+++ PDFParser/metodos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PDFParser
+{
+    public class Metodos
+    {
+        const string archivoDePrueba = "C:\\Users\\laptop1\\Downloads\\PDFS\\Linstal_APRIL_2018_Run.pdf";
+        public static string ExtractTextFromPdf(string path)
+        {
+            PdfReader reader = new PdfReader(path);
+
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+            {
+                text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+            }
+            return text.ToString();
+        }
+
+        
+      
+    }
+}
